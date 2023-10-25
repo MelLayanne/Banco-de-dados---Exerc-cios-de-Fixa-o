ac80a099745f64b11ce869f26d26c4830978d8f9
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-10-24 22:47:43 -0300

    Exercício 1

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -1,1 +1,18 @@
 exerciciosDeFixacao20231024.sql
+
+DELIMITER //
+CREATE TRIGGER Mgm_clientes
+AFTER INSERT ON Clientes
+FOR EACH ROW
+BEGIN
+    DECLARE mensagem TEXT;
+    SET mensagem = 'Cliente adicionado dia: ';
+    SET mensagem = CONCAT(mensagem, NOW());
+    INSERT INTO Auditoria (mensagem) VALUES (mensagem);
+END;
+//
+DELIMITER ;
+
+INSERT INTO Clientes VALUES (4,'Jonas');
+SELECT * FROM Auditoria;
+
